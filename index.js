1389dcad0254e7c7abbcf47fe1951d4d3f68e58f
exuanbo <exuanbo@protonmail.com>
2020-08-26 18:37:44 +0800

    refactor: add JSDoc comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import { createFilter } from '@rollup/pluginutils'
 import minify from 'minify-html-literals'
 
-export default function minifyHTML(pluginOptions = {}) {
-  if (!pluginOptions.filter) {
-    pluginOptions.filter = createFilter(pluginOptions.include, pluginOptions.exclude)
-  }
-
+/**
+ * https://github.com/exuanbo/rollup-plugin-minify-html-template-literals#options
+ * @param {Object} [pluginOptions]
+ * @param {string[]} [pluginOptions.include] - minimatch of files to minify
+ * @param {string[]} [pluginOptions.exclude] - minimatch of files not to minify
+ * @param {Object} [pluginOptions.options] - minify-html-literals options https://www.npmjs.com/package/minify-html-literals#options
+ */
+function minifyHTML(pluginOptions = {}) {
+  pluginOptions.filter = createFilter(pluginOptions.include, pluginOptions.exclude)
   const options = pluginOptions.options || {}
   return {
     name: 'minify-html-template-literals',
@@ -15,3 +19,5 @@ export default function minifyHTML(pluginOptions = {}) {
     }
   }
 }
+
+export default minifyHTML
